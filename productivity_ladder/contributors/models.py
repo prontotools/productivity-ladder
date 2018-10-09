88ad51fbe39ada20ef9141da75cf952a3c40a9c1
saowaluck <pop@prontomaarketing.com>
2018-10-09 09:18:10 +0700

    :sparkles: Add app and model in contributor

--- productivity_ladder/contributors/models.py
+++ productivity_ladder/contributors/models.py
@@ -12,3 +12,10 @@ class Contributor(models.Model):
 
     def __str__(self):
         return f'username {self.username}'
+
+class Contributor(models.Model):
+    name = models.CharField(max_length=100, null=True, blank=True)
+    username = models.CharField(max_length=100, null=False, blank=False, unique=True)
+
+    def __str__(self):
+        return f'username {self.username}'
