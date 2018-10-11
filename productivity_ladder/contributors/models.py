88b172bdde02972f26688919f2be0a496bd999c3
saowaluck <pop@prontomaarketing.com>
2018-10-11 08:47:01 +0700

    :white_mark_checklist: Add model for contributor :panda_face:

--- productivity_ladder/contributors/models.py
+++ productivity_ladder/contributors/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from repositories.models import Repository
+
 
 class Contributor(models.Model):
     name = models.CharField(max_length=100, null=True, blank=True)
@@ -19,3 +21,15 @@ class Contributor(models.Model):
 
     def __str__(self):
         return f'username {self.username}'
+
+class Commit(models.Model):
+    count = models.IntegerField()
+    date_commited = models.DateTimeField()
+    contributor = models.ForeignKey(Contributor, on_delete=models.CASCADE)
+    repository = models.ForeignKey(Repository, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'contributor {self.contributor} of {self.repository}'
+
+    class Meta:
+        unique_together = (('contributor', 'repository'))
