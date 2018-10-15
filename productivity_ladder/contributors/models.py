d58e48d7aec1e9c6b9e77fd90300d8035f35b1f5
saowaluck <pop@prontomaarketing.com>
2018-10-16 00:04:38 +0700

    :fire: remove dulicate model

--- productivity_ladder/contributors/models.py
+++ productivity_ladder/contributors/models.py
@@ -15,12 +15,6 @@ class Contributor(models.Model):
     def __str__(self):
         return f'username {self.username}'
 
-class Contributor(models.Model):
-    name = models.CharField(max_length=100, null=True, blank=True)
-    username = models.CharField(max_length=100, null=False, blank=False, unique=True)
-
-    def __str__(self):
-        return f'username {self.username}'
 
 class Commit(models.Model):
     count = models.IntegerField()
